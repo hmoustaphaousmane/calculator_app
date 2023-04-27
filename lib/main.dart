ee2cde576f4bd10499cdf0c7935f6f2e61489146
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2023-04-27 18:32:19 +0100

    Create all digits and oprations' buttons.

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Calculator (),
+      home: Calculator(),
     );
   }
 }
@@ -22,37 +22,104 @@ class Calculator extends StatefulWidget {
 }
 
 class _CalculatorState extends State<Calculator> {
+  Widget calculatorbtn (String btntext, Color btncolor, Color textcolor) {
+    return Container(
+      child: ElevatedButton (
+        onPressed: () {
+          // TODO Add function for button
+        },
+        style: ButtonStyle (
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(50),
+              side: const BorderSide(color: Colors.white),
+            ),
+          ),
+          backgroundColor: MaterialStateProperty.all(btncolor),
+          padding: MaterialStateProperty.all(const EdgeInsets.all(20)),
+        ),
+        child: Text (
+          btntext,
+          style: TextStyle(fontSize: 35, color: textcolor,),
+        )
+      )
+        
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
-        title: const Text ('Calculator'),
+        title: const Text('Calculator'),
         backgroundColor: Colors.black,
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 5),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            // Calculator display
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Text(
+                    '0',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(color: Colors.white, fontSize: 100),
+                  ),
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                // Call buttons function : calculatorbtn
+                calculatorbtn('AC', Colors.grey, Colors.black),
+                calculatorbtn('+/-', Colors.grey, Colors.black),
+                calculatorbtn('%', Colors.grey, Colors.black),
+                calculatorbtn('/', Colors.amber[700]!, Colors.white),               
+              ],
+            ),
+            const SizedBox(height: 10,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                // Call buttons function : calculatorbtn
+                calculatorbtn('7', Colors.grey[850]!, Colors.black),
+                calculatorbtn('8', Colors.grey[850]!, Colors.black),
+                calculatorbtn('9', Colors.grey[850]!, Colors.black),
+                calculatorbtn('x', Colors.amber[700]!, Colors.white),               
+              ],
+            ),
+            const SizedBox(height: 10,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                // Call buttons function : calculatorbtn
+                calculatorbtn('4', Colors.grey[850]!, Colors.black),
+                calculatorbtn('5', Colors.grey[850]!, Colors.black),
+                calculatorbtn('6', Colors.grey[850]!, Colors.black),
+                calculatorbtn('-', Colors.amber[700]!, Colors.white),               
+              ],
+            ),
+            const SizedBox(height: 10,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                // Call buttons function : calculatorbtn
+                calculatorbtn('1', Colors.grey[850]!, Colors.black),
+                calculatorbtn('2', Colors.grey[850]!, Colors.black),
+                calculatorbtn('3', Colors.grey[850]!, Colors.black),
+                calculatorbtn('+', Colors.amber[700]!, Colors.white),               
+              ],
+            ),
+          ],
         ),
-        body: const Padding (
-          padding: EdgeInsets.symmetric(horizontal: 5),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              // Calculator display
-              SingleChildScrollView (
-                scrollDirection: Axis.vertical,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.all(10.0),
-                      child: Text('0', textAlign: TextAlign.left,
-                        style: TextStyle(color: Colors.white, fontSize: 100),
-                      ),
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
-          ),
+      ),
     );
   }
 }
