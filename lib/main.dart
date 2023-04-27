74a814b31cf558aab6d2502d91b0f55136a01351
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2023-04-27 19:08:25 +0100

    Calculator logic.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -22,29 +22,29 @@ class Calculator extends StatefulWidget {
 }
 
 class _CalculatorState extends State<Calculator> {
-  Widget calculatorbtn (String btntext, Color btncolor, Color textcolor) {
+  Widget calculatorbtn(String btntext, Color btncolor, Color textcolor) {
     return Container(
-      child: ElevatedButton (
-        onPressed: () {
-          // TODO Add function for button
-        },
-        style: ButtonStyle (
-          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-            RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(50),
-              side: const BorderSide(color: Colors.white),
+        child: ElevatedButton(
+            onPressed: () {
+              calculation(btntext);
+            },
+            style: ButtonStyle(
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50),
+                  side: const BorderSide(color: Colors.white),
+                ),
+              ),
+              backgroundColor: MaterialStateProperty.all(btncolor),
+              padding: MaterialStateProperty.all(const EdgeInsets.all(20)),
             ),
-          ),
-          backgroundColor: MaterialStateProperty.all(btncolor),
-          padding: MaterialStateProperty.all(const EdgeInsets.all(20)),
-        ),
-        child: Text (
-          btntext,
-          style: TextStyle(fontSize: 35, color: textcolor,),
-        )
-      )
-        
-    );
+            child: Text(
+              btntext,
+              style: TextStyle(
+                fontSize: 35,
+                color: textcolor,
+              ),
+            )));
   }
 
   @override
@@ -56,20 +56,20 @@ class _CalculatorState extends State<Calculator> {
         backgroundColor: Colors.black,
       ),
       body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 5),
+        padding: const EdgeInsets.symmetric(horizontal: 5),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
             // Calculator display
-            const Row(
+            Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 Padding(
-                  padding: EdgeInsets.all(10.0),
+                  padding: const EdgeInsets.all(10.0),
                   child: Text(
-                    '0',
+                    text,
                     textAlign: TextAlign.left,
-                    style: TextStyle(color: Colors.white, fontSize: 100),
+                    style: const TextStyle(color: Colors.white, fontSize: 100),
                   ),
                 )
               ],
@@ -81,10 +81,12 @@ class _CalculatorState extends State<Calculator> {
                 calculatorbtn('AC', Colors.grey, Colors.black),
                 calculatorbtn('+/-', Colors.grey, Colors.black),
                 calculatorbtn('%', Colors.grey, Colors.black),
-                calculatorbtn('/', Colors.amber[700]!, Colors.white),               
+                calculatorbtn('/', Colors.amber[700]!, Colors.white),
               ],
             ),
-            const SizedBox(height: 10,),
+            const SizedBox(
+              height: 10,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
@@ -92,10 +94,12 @@ class _CalculatorState extends State<Calculator> {
                 calculatorbtn('7', Colors.grey[850]!, Colors.black),
                 calculatorbtn('8', Colors.grey[850]!, Colors.black),
                 calculatorbtn('9', Colors.grey[850]!, Colors.black),
-                calculatorbtn('x', Colors.amber[700]!, Colors.white),               
+                calculatorbtn('x', Colors.amber[700]!, Colors.white),
               ],
             ),
-            const SizedBox(height: 10,),
+            const SizedBox(
+              height: 10,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
@@ -103,10 +107,12 @@ class _CalculatorState extends State<Calculator> {
                 calculatorbtn('4', Colors.grey[850]!, Colors.black),
                 calculatorbtn('5', Colors.grey[850]!, Colors.black),
                 calculatorbtn('6', Colors.grey[850]!, Colors.black),
-                calculatorbtn('-', Colors.amber[700]!, Colors.white),               
+                calculatorbtn('-', Colors.amber[700]!, Colors.white),
               ],
             ),
-            const SizedBox(height: 10,),
+            const SizedBox(
+              height: 10,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
@@ -114,12 +120,153 @@ class _CalculatorState extends State<Calculator> {
                 calculatorbtn('1', Colors.grey[850]!, Colors.black),
                 calculatorbtn('2', Colors.grey[850]!, Colors.black),
                 calculatorbtn('3', Colors.grey[850]!, Colors.black),
-                calculatorbtn('+', Colors.amber[700]!, Colors.white),               
+                calculatorbtn('+', Colors.amber[700]!, Colors.white),
               ],
             ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                // 0 button :
+                ElevatedButton(
+                  style: ButtonStyle(
+                    padding: MaterialStateProperty.all(
+                        const EdgeInsets.fromLTRB(34, 20, 128, 20)),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50),
+                        side: const BorderSide(color: Colors.white),
+                      ),
+                    ),
+                    backgroundColor:
+                        MaterialStateProperty.all(Colors.grey[850]),
+                  ),
+                  onPressed: () {},
+                  child: const Text(
+                    "0",
+                    style: TextStyle(
+                      fontSize: 35,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                calculatorbtn('.', Colors.grey[850]!, Colors.black),
+                calculatorbtn('=', Colors.amber[700]!, Colors.white),
+              ],
+            )
           ],
         ),
       ),
     );
+  }
+
+  // Calculator logic
+  dynamic text = '0';
+  double firstNumber = 0;
+  double secondNumber = 0;
+
+  dynamic result = '';
+  dynamic finalResult = '';
+  dynamic operator = '';
+  dynamic preOperator = '';
+  void calculation(btntext) {
+    if (btntext == 'AC') {
+      text = '0';
+      firstNumber = 0;
+      secondNumber = 0;
+      result = '';
+      finalResult = '0';
+      operator = '';
+      preOperator = '';
+    } else if (operator == '=' && btntext == '=') {
+      if (preOperator == '+') {
+        finalResult = add();
+      } else if (preOperator == '-') {
+        finalResult = sub();
+      } else if (preOperator == 'x') {
+        finalResult = mul();
+      } else if (preOperator == '/') {
+        finalResult = div();
+      }
+    } else if (btntext == '+' ||
+        btntext == '-' ||
+        btntext == 'x' ||
+        btntext == '/' ||
+        btntext == '=') {
+      if (firstNumber == 0) {
+        firstNumber = double.parse(result);
+      } else {
+        secondNumber = double.parse(result);
+      }
+
+      if (operator == '+') {
+        finalResult = add();
+      } else if (operator == '-') {
+        finalResult = sub();
+      } else if (operator == 'x') {
+        finalResult = mul();
+      } else if (operator == '/') {
+        finalResult = div();
+      }
+      preOperator = operator;
+      operator = btntext;
+      result = '';
+    } else if (btntext == '%') {
+      result = firstNumber / 100;
+      finalResult = doesContainDecimal(result);
+    } else if (btntext == '.') {
+      if (!result.toString().contains('.')) {
+        result = result.toString() + '.';
+      }
+      finalResult = result;
+    } else if (btntext == '+/-') {
+      result.toString().startsWith('-')
+          ? result = result.toString().substring(1)
+          : result = '-' + result.toString();
+      finalResult = result;
+    } else {
+      result = result + btntext;
+      finalResult = result;
+    }
+
+    setState(() {
+      text = finalResult;
+    });
+  }
+
+  String add() {
+    result = (firstNumber + secondNumber).toString();
+    firstNumber = double.parse(result);
+    return doesContainDecimal(result);
+  }
+
+  String sub() {
+    result = (firstNumber - secondNumber).toString();
+    firstNumber = double.parse(result);
+    return doesContainDecimal(result);
+  }
+
+  String mul() {
+    result = (firstNumber * secondNumber).toString();
+    firstNumber = double.parse(result);
+    return doesContainDecimal(result);
+  }
+
+  String div() {
+    result = (firstNumber / secondNumber).toString();
+    firstNumber = double.parse(result);
+    return doesContainDecimal(result);
+  }
+
+  String doesContainDecimal(dynamic result) {
+    if (result.toString().contains('.')) {
+      List<String> splitDecimal = result.toString().split('.');
+      if (!(int.parse(splitDecimal[1]) > 0)) {
+        return result = splitDecimal[0].toString();
+      }
+    }
+    return result;
   }
 }
